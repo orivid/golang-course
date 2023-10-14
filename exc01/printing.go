b267811f118c907315d9bf4aae0c70c17545d7b8
ori vider <or197@hotmail.com>
2023-10-14 13:39:51 +0300

    first excercise file

---
+++ exc01/printing.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var collection = "collection"
+
+	fmt.Printf("Hi this is my emoji %s: 😂😊💕", collection)
+
+	fmt.Printf(`
+	Hi this is a string literal program
+	How does it look?
+	
+	
+	It looks great
+   `)
+
+}
